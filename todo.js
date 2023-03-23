3b68d7700da38791051bc1b2f2341aecf980f0a1
Shyamshukla123 <shyamshukla789@gmail.com>
2023-03-23 12:30:33 +0530

    creat todo

---
+++ todo.js
@@ -0,0 +1,111 @@
+let tasks = [];
+
+const InputField = document.getElementById('add');
+const addTaskInput = document.getElementById('btn');
+const tasksCounter = document.getElementById('tasks-counter');
+const tasksList = document.getElementById('list');
+
+// console.log('Working');
+function addtaskToDOM(task){
+        const li =document.createElement('li');
+       li.innerHTML =   `
+    <input type="checkbox" id="${task.id}" ${ task.done ? 'checked':''} class="custom-checkbox" />
+    <label for="${task.id}">${task.text}</label>
+    <img src="delete-forever.png" class="delete" data-id="${task.id}"/>
+    `;
+       
+        // template literals/string
+      tasksList.append(li);
+
+
+        
+}
+function renderList () {
+        tasksList.innerHTML='';
+        for (let i = 0; i < tasks.length; i++) {
+            addtaskToDOM(tasks[i]);
+        }
+        tasksCounter.innerHTML = tasks.length;
+      
+}
+
+function TaskDoneCheck (taskId) {
+    const task=tasks.filter(function(task){
+            return task.id===taskId;
+    });
+    if(task.length>0){
+        const currentTask=task[0];
+        currentTask.done=!currentTask.done;
+        
+        renderList();
+        showNotification("task done succesfully");
+        return;
+    }
+    showNotification("could not done the task");
+}
+
+function deleteTask (taskId) {
+    let newTasks=tasks.filter(function(task){
+        return task.id!==taskId;
+    });
+    tasks=newTasks;
+    renderList();
+    showNotification("Task deleted succesfully");
+}
+
+function addTask (task) {
+    if(task){
+        tasks.push(task);
+        renderList();
+        showNotification('Task added succesfully');
+        return;
+    }
+    showNotification('Task is not added');
+}
+
+function showNotification(text) {
+
+    alert(text);
+}
+
+function handleInputKeypress(e){
+  
+            const text=InputField.value;
+            console.log(text);
+            if(!text){
+                showNotification("input should not be empty");
+                return;
+            }
+
+            const task={
+                text:text,
+                id:Date.now().toString(),
+                done:false,
+            }
+            InputField.value='';
+            addTask(task);
+   
+}
+
+addTaskInput.addEventListener('click',handleInputKeypress);
+
+function HandlerClickListener(e){
+ var target=e.target;
+
+if(target.className=='delete'){
+        const taskId=target.dataset.id;
+        if(window.confirm("do you really wanna delete the task")){
+            deleteTask(taskId);
+        }
+        
+        return;
+    }else if(target.className=='custom-checkbox'){
+        const taskId=target.id;
+        
+        TaskDoneCheck(taskId);
+        return;
+  }
+}
+
+
+document.addEventListener('click',HandlerClickListener)
